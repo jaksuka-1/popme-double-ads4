142e2cc7dd3c1b186768c2d19dc18549afc2badf
jaksuka-1 <71391131+jaksuka-1@users.noreply.github.com>
2023-05-05 20:26:56 +0700

    Create banner-adsterra-pepoontime-728x90.js

---
+++ banner-adsterra-pepoontime-728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'a7adf9d52b6ef836c2a63bc70bb51a59',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://effacedefend.com/a7adf9d52b6ef836c2a63bc70bb51a59/invoke.js"></scr' + 'ipt>');
