94f5750191491f451b35e0b601dcc578126ca39c
jaksuka-1 <71391131+jaksuka-1@users.noreply.github.com>
2023-05-08 08:02:08 +0700

    Create banner-adsterra-suports-jaksuka-728x90.js

---
+++ banner-adsterra-suports-jaksuka-728x90.js
@@ -0,0 +1,8 @@
+  atOptions = {
+		'key' : '816f06b027fa2bd32eafeae7088cad97',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/816f06b027fa2bd32eafeae7088cad97/invoke.js"></scr' + 'ipt>');
