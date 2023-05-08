0d721b73451255a2bff78a69012ac6ae71eee48f
jaksuka-1 <71391131+jaksuka-1@users.noreply.github.com>
2023-05-08 07:59:25 +0700

    Create banner-adsterra-suports-jaksuka-300x250.js

---
+++ banner-adsterra-suports-jaksuka-300x250.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'dfa835ad21fe6c050a2f50e8df0c24dd',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/dfa835ad21fe6c050a2f50e8df0c24dd/invoke.js"></scr' + 'ipt>');
